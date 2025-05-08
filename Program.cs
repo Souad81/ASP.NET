442a929fe4196ba9139538c7bbefa644caf7c7de
Souad81 <suaadalrawi@yahoo.com>
2025-05-08 22:48:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Projects}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
+
